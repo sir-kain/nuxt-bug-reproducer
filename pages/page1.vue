ffa63c34ee95358a7664fff3c22a61a532db97d9
Waly <ahmadouwalyndiaye@gmail.com>
2021-08-15 11:25:15 +0000

    initial commit

---
+++ pages/page1.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nuxt-link to="/page2"> page2 </nuxt-link>
+    <h1>page 1</h1>
+  </div>
+</template>
---
+++ pages/page2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>page 2</h1>
+    <nuxt-link to="/page1">page 1</nuxt-link>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: red;
+}
+</style>
